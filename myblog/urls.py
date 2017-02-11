850fd1efb36c787367c7263591c067c3c868cf5e
ramjanali <aliramjan002@gmail.com>
2017-02-12 00:38:57 +0600

    My Django Girls app, first commit

---
+++ myblog/urls.py
@@ -0,0 +1,18 @@
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from django.contrib.auth import views
+from blog import views as v
+
+urlpatterns = [
+    
+     url(r'^$', v.index),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', views.login, name='login'),
+ url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
+
+ 
+]
